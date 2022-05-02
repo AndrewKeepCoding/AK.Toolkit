e495acfa55eb5f1624d7a66b48bc8ab6d45bed2e
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2022-05-03 05:29:17 +0900

    Add SuggestionForeground Option to the AutoCompleteTextBox sample
    Also created a Page for the samples due to the problem that you currently? cannot use converters on a Window.

---
+++ Samples/AK.Toolkit.Samples/ControlBackgroundGetter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace AK.Toolkit.Samples;
+
+public class ControlBackgroundGetter : IValueConverter
+{
+    public object? Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is Control control)
+        {
+            if (control.Tag is "Default")
+            {
+                return null;
+            }
+
+            return control.Background;
+        }
+
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotImplementedException();
+}
