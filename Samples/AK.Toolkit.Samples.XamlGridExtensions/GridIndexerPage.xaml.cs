c2a87a12c3f33e872d7bc5d6f85d8022f5db1730
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2022-05-21 08:45:30 +0900

    Add sample pages

---
+++ Samples/AK.Toolkit.Samples.XamlGridExtensions/GridIndexerPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace AK.Toolkit.Samples.XamlGridExtensions;
+
+public sealed partial class GridIndexerPage : Page
+{
+    public GridIndexerPage()
+    {
+        this.InitializeComponent();
+        this.RunGridIndexer();
+    }
+}
--- Samples/AK.Toolkit.Samples.XamlGridExtensions/MainWindow.xaml.cs
+++ Samples/AK.Toolkit.Samples.XamlGridExtensions/MainWindow.xaml.cs
@@ -7,6 +7,5 @@ public sealed partial class MainWindow : Window
     public MainWindow()
     {
         this.InitializeComponent();
-        this.RunGridIndexer();
     }
 }
