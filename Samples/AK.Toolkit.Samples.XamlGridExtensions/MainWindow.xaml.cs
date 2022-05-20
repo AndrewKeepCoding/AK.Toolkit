32950a0f0cf10177c1d7e0c53a6cd71237e4f0e5
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2022-05-20 23:30:52 +0900

    Create AK.Toolkit.Samples.XamlGridExtensions project

---
+++ Samples/AK.Toolkit.Samples.XamlGridExtensions/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace AK.Toolkit.Samples.XamlGridExtensions;
+
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        this.InitializeComponent();
+    }
+}
