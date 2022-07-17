8108cf703d9a22ebf3a5c6d5590717496f397c59
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2022-07-17 19:44:45 +0900

    Implement sample code

---
+++ Samples/AK.Toolkit.Samples.Localization/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using AK.Toolkit.WinUI3.Localization;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace AK.Toolkit.Samples.Localization;
+
+public sealed partial class MainPage : Page
+{
+    private readonly ILocalizer _localizer;
+
+    public MainPage()
+    {
+        InitializeComponent();
+        _localizer = Ioc.Default.GetRequiredService<ILocalizer>();
+        Loaded += MainPage_Loaded;
+    }
+
+    private void MainPage_Loaded(object sender, RoutedEventArgs e)
+    {
+        _localizer.RunLocalization(Root);
+    }
+}
--- Samples/AK.Toolkit.Samples.Localization/MainWindow.xaml.cs
+++ Samples/AK.Toolkit.Samples.Localization/MainWindow.xaml.cs
@@ -12,11 +12,13 @@ public sealed partial class MainWindow : Window
 {
     private readonly ILocalizer _localizer;
 
-    public IEnumerable<Tuple<string, string>> AvailableLanguages { get; set; }
+    //public IEnumerable<Tuple<string, string>> AvailableLanguages { get; set; }
 
     public MainWindow()
     {
         InitializeComponent();
+        ExtendsContentIntoTitleBar = true;
+        SetTitleBar(AppTitleBar);
 
         _localizer = Ioc.Default.GetRequiredService<ILocalizer>();
 
@@ -25,26 +27,39 @@ public sealed partial class MainWindow : Window
             _localizer.RegisterRootElement(content);
         }
 
-        AvailableLanguages = _localizer.GetAvailableLanguages()
-            .Select(x =>
-            {
-                string displayName = x;
+        //AvailableLanguages = _localizer.GetAvailableLanguages()
+        //    .Select(x =>
+        //    {
+        //        string displayName = x;
 
-                if (_localizer.GetLocalizedString(x) is string localizedDisplayName)
-                {
-                    displayName = localizedDisplayName;
-                }
+        //        if (_localizer.GetLocalizedString(x) is string localizedDisplayName)
+        //        {
+        //            displayName = localizedDisplayName;
+        //        }
 
-                return new Tuple<string, string>(displayName, x);
-            });
+        //        return new Tuple<string, string>(displayName, x);
+        //    });
     }
 
-    private void RadioButtons_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
     {
-        if (e.AddedItems.Count > 0 &&
-            (e.AddedItems[0] as Tuple<string, string>)?.Item2 is string language)
+        if (args.IsSettingsSelected is true)
         {
-            _localizer.RunLocalizationOnRegisteredRootElements(language);
+            ContentFrame.Navigate(typeof(SettingsPage));
+        }
+        else if (args.SelectedItem is NavigationViewItem item &&
+            Type.GetType("AK.Toolkit.Samples.Localization." + (string)item.Tag) is Type pageType)
+        {
+            ContentFrame.Navigate(pageType);
         }
     }
+
+    //private void RadioButtons_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    //{
+    //    if (e.AddedItems.Count > 0 &&
+    //        (e.AddedItems[0] as Tuple<string, string>)?.Item2 is string language)
+    //    {
+    //        _localizer.RunLocalizationOnRegisteredRootElements(language);
+    //    }
+    //}
 }
