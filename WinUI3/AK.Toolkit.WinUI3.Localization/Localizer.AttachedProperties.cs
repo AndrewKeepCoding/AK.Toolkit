2d42e932751da0ae4d96023012ff78bbe554b416
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2022-11-22 17:28:52 +0900

    Make Uids visible

--- WinUI3/AK.Toolkit.WinUI3.Localization/Localizer.AttachedProperties.cs
+++ WinUI3/AK.Toolkit.WinUI3.Localization/Localizer.AttachedProperties.cs
@@ -1,4 +1,7 @@
 using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AK.Toolkit.WinUI3.Localization;
 
@@ -10,7 +13,15 @@ public partial class Localizer
         typeof(Localizer),
         new PropertyMetadata(default));
 
+    private static HashSet<(string Uid, Type DependencyObjectType)> Uids { get; } = new();
+
+    public static IEnumerable<(string Uid, Type DepdencyObjectType)> GetUids() => Uids.ToList();
+
     public static string GetUid(DependencyObject obj) => (string)obj.GetValue(UidProperty);
 
-    public static void SetUid(DependencyObject obj, string value) => obj.SetValue(UidProperty, value);
+    public static void SetUid(DependencyObject obj, string value)
+    {
+        obj.SetValue(UidProperty, value);
+        _ = Uids.Add((Uid: value, DependencyObjectType: obj.GetType()));
+    }
 }
