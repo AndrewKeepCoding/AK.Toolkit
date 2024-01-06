2b1689983def75020bc128ce222fbeb31d94bf42
Andrew KeepCoding <andrewkeepcoding@gmail.com>
2024-01-06 16:18:31 +0900

    Add TextBlockExSampleApp project

---
+++ Samples/AK.Toolkit.WinUI3.TextBlockExSampleApp/MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+using Microsoft.UI.Xaml;
+
+namespace AK.Toolkit.WinUI3.TextBlockExSampleApp;
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        this.InitializeComponent();
+    }
+}
